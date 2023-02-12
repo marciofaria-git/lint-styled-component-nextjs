74da994933f317b62980b04963fff75e081195a0
devmarciofaira <devmarciofaria@gmail.com>
2023-02-12 19:41:49 -0300

    config the _document for work with SSR stylesheet and add the @types/styled-components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,53 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Head,
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import React from "react";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <React.Fragment key={initialProps.html}>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </React.Fragment>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/Favicon.svg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
